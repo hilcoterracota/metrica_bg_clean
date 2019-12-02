98ec36a40c794077b361595ce2248471d5a258ed
snwnst <nestor-mh@outlook.com>
2019-12-02 11:43:33 -0600

    fecha

--- src/main.py
+++ src/main.py
@@ -14,22 +14,17 @@ while True:
     hora_salida = datetime(ahora.year, ahora.month, ahora.day, hour=18, minute=30)
 
     if ahora >= hora_entrada and ahora <= hora_salida and calendar.day_name[ahora.weekday()] not in ['Saturday','Sunday']  :
-        myclient = pymongo.MongoClient(f'mongodb://192.168.2.2:27017',username="root",password="@H1lcotadmin")
-        mydb = myclient["HTERRACOTA"]
-        mycol = mydb["info_pc"]
-        info_pc_historico = mydb['info_pc_historico']
-        
-        snapshot= []
-        today = date.today()
-
+        myclient = pymongo.MongoClient(f'mongodb://{os.environ["MONGO_URL"]}:27017',username=os.environ["MONGO_USER"],password=os.environ["MONGO_PS"], unicode_decode_error_handler='ignore')
         try:
-            for usuario in mycol.find():
-                print("actualizando pc: "+str(usuario['hostname']))
+            snapshot= []
+            today = date.today()
+            for usuario in myclient["HTERRACOTA"]["info_pc"].find():
                 userId = str(usuario['hostiduiid'])
                 listaprosesos = []
                 listaprosesos_aux  = []
                 nombre_usuario = ""
                 tiempo_uso_global = datetime.strptime('00:00:00', '%H:%M:%S')
+                
                 for proseso in usuario['infoprosses']: 
                     lista_pestanias = []
                     kb_uso_memoria = 0
@@ -57,75 +52,69 @@ while True:
                             "tiempoTotal": str(tiempo_uso_app.strftime("%H:%M:%S")),
                             "estado":proseso['estado'],
                             "ventanas":lista_pestanias,
-                            "fecha":str(str(today.year)+"-"+str(today.month)+"-"+str(today.day))
+                            "fecha":str(today)
                         })
-                        
-                        
+
+
                         time_sub_aux = str(tiempo_uso_app.strftime("%H:%M:%S")).split(":")
                         sub_minutos = (int(time_sub_aux[0])*60)+int(time_sub_aux[1])
                         sub_seconds_aux = (sub_minutos*60)+int(time_sub_aux[2])
                         tiempo_uso_global = tiempo_uso_global + timedelta(seconds=int(sub_seconds_aux))
 
-
                 au = filter(lambda x: x["tiempoTotal"].split(":")[1] != "00", listaprosesos_aux)
-                
+
                 au_au = list(au)
 
                 if(nombre_usuario != ""):
                     nombre_usuario = nombre_usuario.split("\\")[1]
                 else:
                     nombre_usuario = usuario["hostname"]
-                
+
                 snapshot.append({
                     "userId":userId,
                     "usuario":nombre_usuario,
                     "listaprosesos":sorted(au_au, key=lambda element: element['usoMemoria'],reverse=True),
                     "tiempoUsoGlobal": str(tiempo_uso_global)
-                })  
-
+                }) 
+                
             for element in snapshot:
-                usr_htr = info_pc_historico.find_one({'userId': element["userId"]})
+                usr_htr = myclient["HTERRACOTA"]["info_pc_historico"].find_one({'usuario': element["usuario"]})
                 if "None" == str(usr_htr):
-                    info_pc_historico.insert_one({
+                    myclient["HTERRACOTA"]["info_pc_historico"].insert_one({
                         "userId":element["userId"],
                         "usuario":element["usuario"],
                         "historico": element["listaprosesos"]
                     })
                 else:
                     for proseso in element["listaprosesos"]:
-                        data_historica = usr_htr["historico"]
+                        data_historica = myclient["HTERRACOTA"]["info_pc_historico"].find_one({'usuario': element["usuario"]})["historico"]
                         for idxh, elemento_historico in enumerate(usr_htr["historico"]):
                             if elemento_historico["fecha"] == proseso["fecha"] and elemento_historico["nombre"] == proseso["nombre"]:
-                                
                                 tiempoTotalAcumulado = proseso["tiempoTotal"]
-
-
                                 if "tiempoAnterior" not in data_historica[idxh] :
                                     data_historica[idxh]["tiempoAnterior"]=proseso["tiempoTotal"]
-
                                 if proseso["tiempoTotal"] >= data_historica[idxh]["tiempoTotal"]:
                                     tiempoTotalAcumulado = proseso["tiempoTotal"]
                                 else:
                                     h1 = datetime.strptime(tiempoTotalAcumulado, '%H:%M:%S')
-                                    
                                     h2 = str(proseso["tiempoTotal"]).split(":")
                                     h3 = str(data_historica[idxh]["tiempoAnterior"]).split(":")
-
                                     h2_a = ((int(h2[0]))+int(h2[1])*60)+int(h2[2])
                                     h3_a = ((int(h3[0]))+int(h3[1])*60)+int(h3[2])
                                     h1 = h1 + timedelta(seconds=(int(h3_a)-int(h2_a)))
                                     tiempoTotalAcumulado = h1.strftime("%H:%M:%S")
-                                
-                            
                                 data_historica[idxh]["ventanas"] = proseso["ventanas"]
                                 data_historica[idxh]["tiempoTotal"] = tiempoTotalAcumulado
                                 data_historica[idxh]["tiempoAnterior"] = proseso["tiempoTotal"]
                                 data_historica[idxh]["estado"] = proseso["estado"]
-                                
+                                print(f'Actualizando {element["usuario"]} - {elemento_historico["nombre"]}: {tiempoTotalAcumulado}')
+
                         if not list(filter(lambda x: x["nombre"] == proseso["nombre"] and x["fecha"] == str(today), data_historica)):
                             data_historica.append(proseso)
-                        info_pc_historico.update_many({"userId":element["userId"]}, {"$set":{"historico":data_historica}}, upsert=True)
-        
+                            print(f'Agragando {element["usuario"]} - {proseso["nombre"]}: {proseso["tiempoTotal"]}')
+                    
+                        myclient["HTERRACOTA"]["info_pc_historico"].update_many({"usuario":element["usuario"]}, {"$set":{"historico":data_historica}}, upsert=True)
+
         except: 
             print("An exception occurred")
     
